565270a8c53a2de2e3da9b57998b34e005df8d54
Steven Dickinson <steven.dickinson@gmail.com>
2011-07-21 21:47:55 +0100

    add oglconsole

---
+++ 3rdparty/oglconsole/oglconsole.h
@@ -0,0 +1,58 @@
+/* oglconsole -- gpl license here */
+
+#ifndef _OGLCONSOLE_H
+#define _OGLCONSOLE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define OGLCONSOLE_USE_SDL
+
+/* Opaque to you you lowly user */
+typedef struct _OGLCONSOLE_Console *OGLCONSOLE_Console;
+
+/* Initialize/uninitialize OGLConsole */
+OGLCONSOLE_Console OGLCONSOLE_Create();
+void OGLCONSOLE_Destroy(OGLCONSOLE_Console console);
+void OGLCONSOLE_Quit();
+
+/* Set console which has PROGRAMMER focus (not application focus) */
+
+/* This function renders the console */
+void OGLCONSOLE_Draw();
+void OGLCONSOLE_Render(OGLCONSOLE_Console console);
+
+/* Print to the console */
+void OGLCONSOLE_Print(char *s, ...);
+void OGLCONSOLE_Output(OGLCONSOLE_Console console, const char *s, ...);
+
+/* Register a callback with the console */
+void OGLCONSOLE_EnterKey(void(*cbfun)(OGLCONSOLE_Console console, char *cmd));
+
+/* This function tries to handle the incoming SDL event. In the future there may
+ * be non-SDL analogs for input systems such as GLUT. Returns true if the event
+ * was handled by the console. If console is hidden, no events are handled. */
+#if defined(OGLCONSOLE_USE_SDL)
+#include "SDL.h"
+int OGLCONSOLE_SDLEvent(SDL_Event * e);
+#endif
+
+/* Sets the current console for receiving user input */
+void OGLCONSOLE_FocusConsole(OGLCONSOLE_Console console);
+
+/* Sets the current console for making options changes to */
+void OGLCONSOLE_EditConsole(OGLCONSOLE_Console console);
+
+/* Sets the dimensions of the console in lines and columns of characters. */
+void OGLCONSOLE_SetDimensions(int width, int height);
+
+/* Show or hide the console. */
+void OGLCONSOLE_SetVisibility(int visible);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
--- sdf_play/SDFPlay/main.cpp
+++ sdf_play/SDFPlay/main.cpp
@@ -5,34 +5,25 @@
 #include <gl/glu.h>
 #include "SDL.h"
 #include "glsl.h"
-
+#include "oglconsole.h"
 
 SDL_Surface *screen;
 
-int main(int argc, char *argv[])
+OGLCONSOLE_Console console;
+
+GLhandleARB prog;
+
+int done = 0;
+
+
+void drawScene()
 {
-    if ( SDL_Init(SDL_INIT_AUDIO|SDL_INIT_VIDEO) < 0 ) {
-        fprintf(stderr, "Unable to init SDL: %s\n", SDL_GetError());
-        return 0;
-    }
-
-	screen = SDL_SetVideoMode(800, 600, 32, SDL_HWSURFACE | SDL_OPENGL);
-	
-	glViewport(0, 0, 800, 600);
-	glShadeModel(GL_SMOOTH);
-	glClearColor(0.2f, 0.2f, 0, 1);
-	glClearDepth(1.0f);
-	glEnable(GL_DEPTH_TEST);
-	glDepthFunc(GL_LEQUAL);
-
-	glMatrixMode(GL_PROJECTION);
-	glOrtho(0, 800, 0, 600, 0, 1);
-
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glEnable(GL_COLOR_MATERIAL);
+
+	glUseProgramObjectARB(0);
 
 	glBegin(GL_TRIANGLES);
 
@@ -43,12 +34,6 @@ int main(int argc, char *argv[])
 	glEnd();
 	glFlush();
 
-	init_glsl();
-
-	const GLcharARB *progVert = "varying vec4 vcol; void main() { gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex; vcol = gl_Color; }" ;
-	const GLcharARB *progFrag = "varying vec4 vcol; void main() { gl_FragColor = vcol * vec4(0.5,0.5,0.5,1.0); }";
-	GLhandleARB prog = createShaderFromProgs(progVert, progFrag);
-
 	glUseProgramObjectARB(prog);
 
 	glBegin(GL_TRIANGLES);
@@ -58,39 +43,116 @@ int main(int argc, char *argv[])
 	glColor3f(0, 1, 1); glVertex3f(200, 110, 0);
 
 	glEnd();
-	glFlush();
+}
+
+void drawConsole()
+{
+	glUseProgramObjectARB(0);
+	OGLCONSOLE_Draw();
+}
+
+void cmdCb(OGLCONSOLE_Console console, char *cmd)
+{
+    if (!strncmp(cmd, "quit", 4)) done = TRUE;
+
+    /*else if (!strncmp(cmd, "add", 3))
+    {
+        int a, b;
+        if (sscanf(cmd, "add %i %i", &a, &b) == 2)
+        {
+            OGLCONSOLE_Output(console, "%i + %i = %i\n", a, b, a+b);
+            return;
+        }
+
+        OGLCONSOLE_Output(console, "usage: add INT INT\n");
+        return;
+    }*/
+    
+    OGLCONSOLE_Output(console, "I don't understand this command: %s\n", cmd);
+}
+
+int main(int argc, char *argv[])
+{
+    if ( SDL_Init(SDL_INIT_EVERYTHING) < 0 ) {
+        fprintf(stderr, "Unable to init SDL: %s\n", SDL_GetError());
+        return 0;
+    }
+
+	screen = SDL_SetVideoMode(800, 600, 32, SDL_HWSURFACE | SDL_OPENGL);
+	
+	glViewport(0, 0, 800, 600);
+	glShadeModel(GL_SMOOTH);
+	glClearColor(0, 0, 0, 1);
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glEnable(GL_COLOR_MATERIAL);
+
+
+	glMatrixMode(GL_PROJECTION);
+	glOrtho(0, 800, 0, 600, 0, 1);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	console = OGLCONSOLE_Create();
+	OGLCONSOLE_EnterKey(cmdCb);
+	OGLCONSOLE_Print("SDF PLAY V0.1 (LOL)");
+
+	init_glsl();
+
+	const GLcharARB *progVert = "varying vec4 vcol; void main() { gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex; vcol = gl_Color; }" ;
+	const GLcharARB *progFrag = "varying vec4 vcol; void main() { gl_FragColor = vcol * vec4(0.5,0.5,0.5,1.0); }";
+	prog = createShaderFromProgs(progVert, progFrag);
+
 
     SDL_Event event;
 
-	SDL_GL_SwapBuffers();
 
-
-    while ( SDL_WaitEvent(&event) ) {
-        switch (event.type) {
-            case SDL_MOUSEMOTION:
-                printf("Mouse moved by %d,%d to (%d,%d)\n", 
-                       event.motion.xrel, event.motion.yrel,
-                       event.motion.x, event.motion.y);
-                break;
-            case SDL_MOUSEBUTTONDOWN:
-                printf("Mouse button %d pressed at (%d,%d)\n",
-                       event.button.button, event.button.x, event.button.y);
-                break;
-			case SDL_KEYDOWN:
-				if (event.key.keysym.sym == SDLK_ESCAPE) {
-					SDL_Event qe;
-					qe.quit.type = SDL_QUIT;
-					SDL_PushEvent(&qe);
-				}
-				break;
-            case SDL_QUIT:
-                exit(0);
-        }
-    }
-    
-    
-	
 	atexit(SDL_Quit);
+
+	while ( !done ) {
+		while ( SDL_PollEvent(&event) ) {
+			if (OGLCONSOLE_SDLEvent(&event) == 0) 
+			{
+				switch( event.type ) 
+				{
+					case SDL_MOUSEMOTION:
+						printf("Mouse moved by %d,%d to (%d,%d)\n", 
+							   event.motion.xrel, event.motion.yrel,
+							   event.motion.x, event.motion.y);
+						break;
+					case SDL_MOUSEBUTTONDOWN:
+						printf("Mouse button %d pressed at (%d,%d)\n",
+							   event.button.button, event.button.x, event.button.y);
+						break;
+					case SDL_KEYDOWN:
+						if (event.key.keysym.sym == SDLK_BACKQUOTE) 
+						{
+							OGLCONSOLE_SetVisibility(1);
+						}
+
+						if (event.key.keysym.sym == SDLK_ESCAPE) 
+						{
+							SDL_Event qe;
+							qe.quit.type = SDL_QUIT;
+							SDL_PushEvent(&qe);
+						}
+						break;
+					case SDL_QUIT:
+						done = 1;
+						break;
+				}
+			}
+		}
+		drawScene();
+		drawConsole();
+		SDL_GL_SwapBuffers();
+	}
+
+	OGLCONSOLE_Destroy(console);
+	SDL_Quit();
+	    
 
 	return 0;   
 }
