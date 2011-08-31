d674d019913f08f7f980113793d3d249f7c73238
Steven Dickinson <steven@piriltd.com>
2011-08-31 11:48:03 +0100

    osx opengl bindigns

--- common/Sys.h
+++ common/Sys.h
@@ -1,20 +1,28 @@
 #ifndef SDF_PLAY_SYS_H_
 #define SDF_PLAY_SYS_H
 
-#ifdef WIN32
-#include <windows.h>
-#include <GL/gl.h>
-#include <GL/glu.h>
-#include <SDL.h>
-
-#ifndef APIENTRYP 
-#define APIENTRYP APIENTRY *
+#ifdef __MACH__
+#  include <OpenGL/gl.h>
+#  include <OpenGL/glu.h>
+#else
+#  ifdef WIN32
+#    include <windows.h>
+#    include <GL/gl.h>
+#    include <GL/glu.h>
+#  else
+#    include <GL/gl.h>
+#    include <GL/glu.h>
+#  endif
 #endif
 
+
+#ifdef WIN32
+#  include <SDL.h>
+#  ifndef APIENTRYP 
+#    define APIENTRYP APIENTRY *
+#  endif
 #else
-#include <GL/gl.h>
-#include <GL/glu.h>
-#include <SDL/SDL.h>
+#  include <SDL/SDL.h>
 #endif
 
 
